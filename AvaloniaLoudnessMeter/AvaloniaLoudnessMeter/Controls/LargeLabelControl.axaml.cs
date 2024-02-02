5734ec217e1377e1dfbf92c0c4f8a8e2b85a9593
Salvatore Campisano <1342203+sal-Camp@users.noreply.github.com>
2024-02-01 20:56:47 -0500

    Initial UI design

---
+++ AvaloniaLoudnessMeter/AvaloniaLoudnessMeter/Controls/LargeLabelControl.axaml.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+
+namespace AvaloniaLoudnessMeter.Controls;
+
+public class LargeLabelControl : TemplatedControl
+{
+    public static readonly StyledProperty<string> LargeTextProperty = AvaloniaProperty.Register<LargeLabelControl, string>(
+        nameof(LargeText), "Large Text");
+
+    public string LargeText
+    {
+        get => GetValue(LargeTextProperty);
+        set => SetValue(LargeTextProperty, value);
+    }
+
+    public static readonly StyledProperty<string> SmallTextProperty = AvaloniaProperty.Register<LargeLabelControl, string>(
+        nameof(SmallText), "small text");
+
+    public string SmallText
+    {
+        get => GetValue(SmallTextProperty);
+        set => SetValue(SmallTextProperty, value);
+    }
+}
---
+++ AvaloniaLoudnessMeter/AvaloniaLoudnessMeter/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AvaloniaLoudnessMeter.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string _boldTitle = "AVALONIA";
+
+    [ObservableProperty]
+    private string _regularTitle = "LOUDNESS METER";
+}
